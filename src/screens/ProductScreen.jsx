cd13ffefc6db5371fca78d26b1b6588416a3f95a
Kunal <coolkunalgupta123@gmail.com>
2022-06-09 02:45:04 +0530

    made changes to get data from backend api

--- src/screens/ProductScreen.jsx
+++ src/screens/ProductScreen.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import { Link, useParams } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
+import { Row, Col, Image, ListGroup, Button } from 'react-bootstrap'
+import axios from 'axios'
 import Rating from '../components/Rating'
-import products from '../products'
 function ProductScreen() {
-    const { id } = useParams();
-    console.log(typeof(id))
-    const product = products.find((p) => p._id === id)
+
+    const [product, setProduct] = useState([])
+    const { id } = useParams()
+
+    useEffect(() => {
+      async function fetchProduct() {
+        const { data } = await axios.get(`/api/products/${id}`)
+        setProduct(data)
+      }
+  
+      fetchProduct()
+    },[id])
+
   return (
     <div>
           <Link to='/' className='btn btn-outline-light my-3'>Go Back</Link>
